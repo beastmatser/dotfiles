652ad8e9be460654516eef69717f30c4fc2f0005
beastmatser <79206232+beastmatser@users.noreply.github.com>
2022-01-22 19:28:15 +0100

    :sparkles: Add .p10k.zsh dotfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 ln -s ~/dotfiles/.editorconfig ~/.editorconfig
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore ~/.gitignore
-ln -s ~/dotfiles/.zsh_history ~/.zsh_history
+ln -s ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -s ~/dotfiles/.zshrc ~/.zshrc

3ea816b1bb664bbf564ad425ed6e05a893852580
beastmatser <79206232+beastmatser@users.noreply.github.com>
2022-01-22 17:45:42 +0100

    :recycle: Refactor to dotfiles

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+ln -s ~/dotfiles/.editorconfig ~/.editorconfig
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore ~/.gitignore
+ln -s ~/dotfiles/.zsh_history ~/.zsh_history
+ln -s ~/dotfiles/.zshrc ~/.zshrc

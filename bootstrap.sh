d41f7cff350362b6c14ceef22ea85092c066c4cc
beastmatser <79206232+beastmatser@users.noreply.github.com>
2022-01-25 18:51:31 +0100

    :art: Format .bash_aliases

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
+ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/.editorconfig ~/.editorconfig
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore ~/.gitignore
 ln -s ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+mkdir -p ~/.config/nvim
+ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
+ls -s ~/dotfiles/init.vim ~/init.vim

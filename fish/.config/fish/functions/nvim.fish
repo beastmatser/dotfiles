3413d6a0375c0270f3d17602410cc4e908b6e11c
beastmatser <79206232+beastmatser@users.noreply.github.com>
2022-04-19 21:08:06 +0200

    :boom: Replace zsh with fish

---
+++ fish/.config/fish/functions/nvim.fish
@@ -0,0 +1,3 @@
+function nvim --description 'alias nvim=nvim -u ~/init.vim'
+ command nvim -u ~/init.vim $argv; 
+end
--- zsh/.config/zsh/aliases.zsh
+++
@@ -1,4 +0,0 @@
-alias 'nvim'='nvim -u ~/init.vim'
-alias 'vi'='nvim'
-alias 'ls'='ls --color=auto'
-alias 'la'='ls -a'
